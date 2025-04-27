c0a272607c5fae92df27bff86f7c56606d710f00
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2025-04-27 17:15:59 -0300

    Entity de Colaboradores

---
+++ src/colaboradores/colaborador.entity.ts
@@ -0,0 +1,37 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { TipoPermissao } from 'src/enums/tipo-permissao.enum';
+import { Solicitacao } from 'src/solicitacoes/solicitaçoes.entity';
+
+@Entity()
+export class Colaborador {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  matricula: string;
+
+  @Column()
+  nome: string;
+
+  @Column()
+  cpf: string;
+
+  @Column()
+  cargo: string;
+
+  @Column()
+  setor: string;
+
+  @Column({ default: false })
+  lideranca: boolean;
+
+  @Column({
+    type: 'enum',
+    enum: TipoPermissao,
+    default: TipoPermissao.COLABORADOR,
+  })
+  permissao: TipoPermissao;
+
+  @OneToMany(() => Solicitacao, solicitacao => solicitacao.colaborador)
+  solicitacoes: Solicitacao[];
+}
